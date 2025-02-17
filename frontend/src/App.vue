956e00034a15105abede9c445a4c16c75e9427c7
DlekiYa <dlekiya@yandex.ru>
2025-02-17 22:26:53 +0300

    add prototype signin page

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,29 @@
 <script setup lang="ts">
-  import { RouterLink, RouterView } from 'vue-router'
-  //import HelloWorld from './components/HelloWorld.vue'
-  import SidePanel from './components/SidePanel.vue';
-  import Header from './components/Header.vue';
-  import Page from './components/Page.vue';
+  import {ref, computed} from 'vue'
+  
+  import Home from './components/Home.vue'
+  import SigninPage from './components/SigninPage.vue';
+  import NotFound from './components/NotFound.vue';
+
+  const routes : { [key: string]: any } = {
+  '/': Home,
+  '/signin': SigninPage
+  }
+
+  const currentPath = ref(window.location.pathname)
+
+  window.addEventListener('pathchange', () => {
+    currentPath.value = window.location.pathname
+  })
+
+  const currentView = computed(() => {
+    console.log(currentPath.value)
+    return routes[currentPath.value.slice(0) || '/'] || NotFound
+  })
 </script>
 
 <template>
-  <Header></Header>
-  <SidePanel></SidePanel>
-  <Page></Page>
+  <component :is="currentView" />
 </template>
 
 <style>
--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,6 +1,16 @@
+<script setup lang="ts">
+    function redirect(endpoint : string) {
+        location.replace(endpoint)
+    }
+</script>
+
 <template>
     <header class="header">
-        <div class="logo">=)</div>
+        <div class="logo">シ</div>
+        <div class="authbox">
+            <div class="signin" @click="redirect('/signin')"> sign in</div>
+            <div class="signup"> sign up </div>
+        </div>
     </header>
 </template>
 
@@ -9,7 +19,8 @@
         position: absolute;
         top: 0px;
         left: 0px;
-        display: flex;
+        display: grid;
+        grid-template-columns: 85% 15%;
         justify-content: left; /* Centers the logo horizontally */
         align-items: center; /* Centers the logo vertically */
         background-color: #f8f8f8; /* Light background color */
@@ -21,5 +32,32 @@
     .logo img {
         height: 50px; /* Adjust the height of the logo */
         width: auto; /* Keeps the aspect ratio */
+    }
+
+    .authbox {
+        justify-content: center;
+        display: grid;
+        grid-template-columns: 50% 50%;
+    }
+
+    .signin {
+        border: 5px solid #105010;
+        padding-inline: 10px;
+        border-radius: 10px;
+        background-color: #105010;
+        color: white;
+        justify-self: center;
+        font-weight: bold;
+    }
+
+    .signup {
+        border: 2px solid black;
+        padding-inline: 10px;
+        border-radius: 10px;
+        background-color: #e0e0e0;
+        color: #105010;
+        justify-self: center;
+        align-items: center;
+        justify-content: center;
     }
 </style>
